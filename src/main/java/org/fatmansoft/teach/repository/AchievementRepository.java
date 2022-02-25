aea993d15d02749e47fcc1a6a9841749b777ab6a
guosurui <13012615613@163.com>
2022-02-26 00:17:45 +0800

    feat: 实现Achievement类
    实现Achievement类的field和setter&getter

--- src/main/java/org/fatmansoft/teach/repository/AchievementRepository.java
+++ src/main/java/org/fatmansoft/teach/repository/AchievementRepository.java
@@ -3,5 +3,6 @@ package org.fatmansoft.teach.repository;
 import org.fatmansoft.teach.models.Achievement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface AchievementRepository extends JpaRepository<Achievement, Integer> {
 }
