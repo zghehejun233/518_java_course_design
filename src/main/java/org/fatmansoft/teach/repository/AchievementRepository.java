7628c516f456ee5ffb77cebfbe9679424003d844
guosurui <13012615613@163.com>
2022-02-26 01:22:05 +0800

    feat: 实现achievement的Init显示接口
    实现achievement的Init显示接口

--- src/main/java/org/fatmansoft/teach/repository/AchievementRepository.java
+++ src/main/java/org/fatmansoft/teach/repository/AchievementRepository.java
@@ -1,8 +1,15 @@
 package org.fatmansoft.teach.repository;
 
 import org.fatmansoft.teach.models.Achievement;
+import org.fatmansoft.teach.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 public interface AchievementRepository extends JpaRepository<Achievement, Integer> {
+    @Query(value = "from Achievement where ?1='' or studentNum like %?1% or studentName like %?1% ")
+    List<Achievement> findAchievementListByNumName(String numName);
+
 }
