a73a06634ff05f2984e968cd0204fad35ebbca0f
guosurui <13012615613@163.com>
2022-02-26 10:55:12 +0800

    fix: 完善achievement相关功能现状
    进行注释补充和测试
    bug: achievement表查询错误

--- src/main/java/org/fatmansoft/teach/repository/AchievementRepository.java
+++ src/main/java/org/fatmansoft/teach/repository/AchievementRepository.java
@@ -1,7 +1,6 @@
 package org.fatmansoft.teach.repository;
 
 import org.fatmansoft.teach.models.Achievement;
-import org.fatmansoft.teach.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,7 +8,5 @@ import java.util.List;
 
 
 public interface AchievementRepository extends JpaRepository<Achievement, Integer> {
-    @Query(value = "from Achievement where ?1='' or studentNum like %?1% or studentName like %?1% ")
-    List<Achievement> findAchievementListByNumName(String numName);
 
 }
