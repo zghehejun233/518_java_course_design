252e0e9dfc50705d17c1150c34b0eee1ea64e14b
guosurui <13012615613@163.com>
2022-03-07 21:48:35 +0800

    fix: 修复数据表创建失败的bug
    又进行了修复

--- src/main/java/org/fatmansoft/teach/models/Attendance.java
+++ src/main/java/org/fatmansoft/teach/models/Attendance.java
@@ -7,19 +7,18 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
 @Table(name = "attendance",uniqueConstraints = {})
 @Setter
 @Getter
-public class Attendance {
+public class Attendance{
     @Id
     private Integer id;
-    @NotBlank
     private String studentName;
     private String courseName;
+    private Boolean checkState;
     private Date time;
-    private String check;
+
 }
