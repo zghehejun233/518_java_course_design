2785d0e8982b1a484924e7a021f5d89524f1bc24
nnnn0907 <A13418791819@163.com>
2022-04-09 20:07:49 +0800

    浅浅测试一下
    Signed-off-by: nnnn0907 <A13418791819@163.com>

--- src/main/java/org/fatmansoft/teach/SpringBootSecurityJwtApplication.java
+++ src/main/java/org/fatmansoft/teach/SpringBootSecurityJwtApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootSecurityJwtApplication {
-
+    //这是一个测试
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
     }
